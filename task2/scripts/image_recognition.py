7a3ddedb0837663c240a894c78ca0829fdbbf8c5
Aljaž Justin <80422513+AljazJus@users.noreply.github.com>
2023-05-21 18:16:35 +0200

    added some fix
    added some fix

--- task2/scripts/image_recognition.py
+++ task2/scripts/image_recognition.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
 import rospy
+import sys
+import cv2
 from sensor_msgs.msg import Image
-from import ImageRecognition, ImageRecognitionResponse
+from task2.srv import ImageRecognition, ImageRecognitionResponse
+from cv_bridge import CvBridge, CvBridgeError
 
 class ImageRecognitionServer:
     def __init__(self):
         # Initialize the ROS node
         rospy.init_node('image_recognition_server')
+        self.bridge = CvBridge()
 
         # Subscribe to the image topic
-        self.image_subscriber = rospy.Subscriber('/camera/image_raw', Image, self.image_callback)
+        self.image_subscriber = rospy.Subscriber('/camera/rgb/image_raw', Image, self.image_callback)
 
         # Wait for the first image to arrive
         self.latest_image = None
@@ -22,21 +26,27 @@ class ImageRecognitionServer:
 
     def image_callback(self, msg):
         # Store the latest image
-        self.latest_image = msg
+
+        try:
+            self.latest_image = self.bridge.imgmsg_to_cv2(msg, "rgb8")
+        except CvBridgeError as e:
+            print(e)
+            return
 
         # Unsubscribe from the image topic
         self.image_subscriber.unregister()
 
     def image_recognition_callback(self, req):
         # Retrieve the latest image
-        image_data = self.latest_image.data
-
+    
         # Process the image data here
         # ...
 
         # Return the recognition results in the response
         response = ImageRecognitionResponse()
-        response.recognition_results = "Object detected"
+        response.wonted = False
+        response.prize=0
+        response.color = ""
         return response
 
     def run(self):
