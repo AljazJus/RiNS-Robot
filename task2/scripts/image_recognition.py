010ad5268c48f56d2b0b17500308181d55d36fd5
Aljaž Justin <80422513+AljazJus@users.noreply.github.com>
2023-05-24 18:08:00 +0200

    image reading finito
    image reading finito

--- task2/scripts/image_recognition.py
+++ task2/scripts/image_recognition.py
@@ -9,21 +9,28 @@ from cv_bridge import CvBridge, CvBridgeError
 import pytesseract
 import numpy as np
 
+import pytesseract
+from PIL import Image as PILImage
+
+import cv2.aruco as aruco
+import sys
+
+import os
+
+
+
 class ImageRecognitionServer:
     def __init__(self):
         # Initialize the ROS node
         rospy.init_node('image_recognition_server')
         self.bridge = CvBridge()
 
+        script_path = os.path.abspath(__file__)
+        self.script_dir = os.path.dirname(script_path)
+        print("The script is located at:", self.script_dir)
         # Subscribe to the image topic
 
         # Wait for the first image to arrive
-        
-
-
-        self.dictm = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
-
-        self.params =  cv2.aruco.DetectorParameters_create()
 
         # Create the image recognition service
         self.image_recognition_service = rospy.Service('image_recognition', ImageRecognition, self.image_recognition_callback)
@@ -39,109 +46,80 @@ class ImageRecognitionServer:
             return 0
         
         try:
-            rgb_image = self.bridge.imgmsg_to_cv2(img, "bgr8")
+            cv_image = self.bridge.imgmsg_to_cv2(img, "bgr8")
         except CvBridgeError as e:
             print(e)
-
+        
         response = ImageRecognitionResponse()
-        response.wonted = False
-        response.prize=0
-        response.color = ""
-        # Retrieve the latest image
 
-        # Process the image data here
-        # ...
-        corners, ids, rejected_corners = cv2.aruco.detectMarkers(rgb_image,self.dictm,parameters=self.params)
-        
-        # Increase proportionally if you want a larger image
-        image_size=(351,248,3)
-        marker_side=50
+        # Load the face detection classifier
+        face_cascade = cv2.CascadeClassifier(self.script_dir+'/haarcascade_frontalface_default.xml')
+        if face_cascade.empty():
+            print("Error: Failed to load face detection classifier")
+            raise Exception("Failed to load face detection classifier")
 
-        img_out = np.zeros(image_size, np.uint8)
-        out_pts = np.array([[marker_side/2,img_out.shape[0]-marker_side/2],
-                        [img_out.shape[1]-marker_side/2,img_out.shape[0]-marker_side/2],
-                        [marker_side/2,marker_side/2],
-                        [img_out.shape[1]-marker_side/2,marker_side/2]])
+        # Convert the input image to grayscale
+        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-        src_points = np.zeros((4,2))
-        cens_mars = np.zeros((4,2))
+        # Detect faces in the input image
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-        if not ids is None:
-            if len(ids)==4:
-                print('4 Markers detected')
+        # If no faces are detected, set the response fields and return
+        if len(faces) == 0:
+            response.wonted = False
+            response.prize = 0
+            response.color = ""
+            return response
 
-                cen_point = []
-                for idx in ids:
-                    # Calculate the center point of all markers
-                    cors = np.squeeze(corners[idx[0]-1])
-                    cen_mar = np.mean(cors,axis=0)
-                    cens_mars[idx[0]-1]=cen_mar
-                    cen_point = np.mean(cens_mars,axis=0)
+        # Find the face with the largest area
+        best_face = None
+        max_area = 0
+        for face in faces:
+            area = face[2] * face[3]
+            if area > max_area:
+                max_area = area
+                best_face = face
+
+        # Extract the region below the face from the input image
+        (x, y, w, h) = best_face
+        region_below_face = gray[y+h:y+h+100, x-50:x+w+50]
+
+        scale_factor = 3
+        region_below_face = cv2.resize(region_below_face, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_CUBIC)
+
+        thresh_value = 100
+        block_size = 11
+        # Apply thresholding to isolate the number and word regions
+        thresh = cv2.adaptiveThreshold(region_below_face, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, blockSize=block_size, C=2)
+        thresh[thresh < thresh_value] = 0
+
+        # Recognize the text using Tesseract
+        number_text = pytesseract.image_to_string(thresh)
+        # print(number_text)
+        cv2.imshow('Poster', thresh)
+        cv2.waitKey(0)
+        # Filter the recognized text to extract words and digits
+
+        digits = ""
+        color = ""
+        for s in number_text.split():
+            if s.isdigit():
+                digits += s
+            elif s.lower() in ["green", "red", "yellow", "blue"]:
+                color = s.lower()
             
-                for coords in cens_mars:
-                    #  Map the correct source points
-                    if coords[0]<cen_point[0] and coords[1]<cen_point[1]:
-                        src_points[2]=coords
-                    elif coords[0]<cen_point[0] and coords[1]>cen_point[1]:
-                        src_points[0]=coords
-                    elif coords[0]>cen_point[0] and coords[1]<cen_point[1]:
-                        src_points[3]=coords
-                    else:
-                        src_points[1]=coords
+            # print("Unknown word: " + s)
 
-                h, status = cv2.findHomography(src_points, out_pts)
-                img_out = cv2.warpPerspective(cv_image, h, (img_out.shape[1],img_out.shape[0]))
-                
-                ################################################
-                #### Extraction of digits starts here
-                ################################################
-                
-                # Cut out everything but the numbers
-                img_out = img_out[125:221,50:195,:]
-                
-                # Convert the image to grayscale
-                img_out = cv2.cvtColor(img_out, cv2.COLOR_BGR2GRAY)
-                
-                # Option 1 - use ordinairy threshold the image to get a black and white image
-                #ret,img_out = cv2.threshold(img_out,100,255,0)
+        words = number_text.split()
 
-                # Option 1 - use adaptive thresholding
-                img_out = cv2.adaptiveThreshold(img_out,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,5)
-                
-                # Use Otsu's thresholding
-                #ret,img_out = cv2.threshold(img_out,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-                
-                # Pass some options to tesseract
-                config = '--psm 13 outputbase nobatch digits'
-                
-                # Visualize the image we are passing to Tesseract
-                cv2.imshow('Warped image',img_out)
-                cv2.waitKey(1)
-            
-                # Extract text from image
-                text = pytesseract.image_to_string(img_out, config = config)
-                
-                # Check and extract data from text
-                print('Extracted>>',text)
-                
-                # Remove any whitespaces from the left and right
-                text = text.strip()
-                
-        #             # If the extracted text is of the right length
-                if len(text)==2:
-                    x=int(text[0])
-                    y=int(text[1])
-                    print('The extracted datapoints are x=%d, y=%d' % (x,y))
-                else:
-                    print('The extracted text has is of length %d. Aborting processing' % len(text))
-                
-            else:
-                print('The number of markers is not ok:',len(ids))
-        else:
-            print('No markers found')
-        # Return the recognition results in the response
-        
+        # Set the response fields
+        response.wonted = True
+        response.prize = int(digits)
+        response.color = color
+
         return response
+
+
 
     def run(self):
         # Spin the node to receive messages
