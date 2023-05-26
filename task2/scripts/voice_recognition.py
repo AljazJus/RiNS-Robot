a7423e6f3b461d45d69547773663861cbb3ed93c
Edin Ćehić <edinehi53@gmail.com>
2023-05-26 14:49:23 +0200

    Changed voice_recognition to service
    subscribe to "/potential_cylinders"  to get string of colors seperated by , se prau "green,red,blue"
    
    publishas True na "/voice_initializer" da ga poženeš

--- task2/scripts/voice_recognition.py
+++ task2/scripts/voice_recognition.py
@@ -1,78 +1,96 @@
+#!/usr/bin/python3
+
+
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Bool
 # import speech_recognition from ros-vosk package
 from ros_vosk.msg import speech_recognition
 import re
 
-def recognize_voice():
-    """
-    This function recognizes the voice from the microphone using ros-vosk library.
-    It returns the colors of the cylinders with suspected thieves.
-    """
-    try:
-        print("I have begun listening to your voice")
-        # if it times out, open a ROS text terminal and wait for the user to write the colors of the cylinders with suspected thieves
-        message = rospy.wait_for_message("/speech_recognition/final_result", String, timeout=40)
-        colors = extract_possible_cylinders(message)
-        if len(colors) == 0:
-            print("No cylinders found")
+class voice_detector():
+    def __init__(self):
+        rospy.init_node('voice_detector', anonymous=True)
+
+        self.voice_initializer = rospy.Subscriber("/voice_initializer", Bool, self.initialize_listen)
+        ## publish a array of colors
+        self.voice_pub = rospy.Publisher("/potential_cylinders", String, queue_size=1)
+        self.colors = []
+    
+    def initialize_listen(self, msg):
+        if msg.data:
+            print("I have begun listening to your voice")
             try:
+                message = rospy.wait_for_message("/speech_recognition/final_result", String, timeout=10)
+                print("I have heard you say: ", message)
+                colors = self.extract_possible_cylinders(message, self.colors)
+                if len(colors) == 0:
+                    print("No cylinders found")
+                    # open text input terminal and wait for the user to write the colors of the cylinders with suspected thieves
+                    message = input("Can you tell me where the thieves are?")
+                    colors = self.extract_possible_cylinders(message, self.colors)
+                    if len(colors) == 0:
+                        self.voice_pub.publish("")
+                        return
+                    else:
+                        self.voice_pub.publish(",".join(colors))
+                        return
+                else:
+                    self.voice_pub.publish(",".join(colors))
+                    return
+            except rospy.exceptions.ROSException as e:
+                print("Timeout occurred:", e)
                 # open text input terminal and wait for the user to write the colors of the cylinders with suspected thieves
                 message = input("Can you tell me where the thieves are?")
-                colors = extract_possible_cylinders(message)
+
+                colors = self.extract_possible_cylinders(message, self.colors)
                 if len(colors) == 0:
-                    return False
+                    self.voice_pub.publish("")
+                    return
                 else:
-                    return colors
-            except KeyboardInterrupt:
-                print("User input interrupted")
-                return False
+                    self.voice_pub.publish(",".join(colors))
+                    return
+    
+    
+    def extract_possible_cylinders(self, msg, colors = []):
+        #check if msg is string or has msg.data attribute
+        print("Extracting possible cylinders")
+
+        if isinstance(msg, str):
+            pattern = re.compile(r'\b(yellow|green|red|blue|Yellow|Green|Red|Blue)\b')
+            matches = re.findall(pattern, msg)
+            
+            for match in matches:
+                if match not in colors:
+                    color = match.split()[0]
+                    colors.append(color)
+            
+            if not colors:
+                return []
+            
         else:
-            return colors
-    except rospy.exceptions.ROSException as e:
-        print("Timeout occurred:", e)
-        try:
-            # open text input terminal and wait for the user to write the colors of the cylinders with suspected thieves
-            message = input("Can you tell me where the thieves are?")
-            colors = extract_possible_cylinders(message)
-            if len(colors) == 0:
-                return False
-            else:
-                return colors
-        except KeyboardInterrupt:
-            print("User input interrupted")
-            return False
+            pattern = re.compile(r'\b(yellow|green|red|blue|Yellow|Green|Red|Blue)\b')
+            matches = re.findall(pattern, msg.data)
 
-    
-    
-def extract_possible_cylinders(msg, colors = []):
-    #check if msg is string or has msg.data attribute
-    if isinstance(msg, str):
-        pattern = re.compile(r'\b(yellow|green|red|blue)\b')
-        matches = re.findall(pattern, msg)
+            for match in matches:
+                if match not in colors:
+                    color = match.split()[0]
+                    colors.append(color)
+
+            if not colors:
+                return []
         
-        for match in matches:
-            if match not in colors:
-                colors.append(match)
-        
-        if not colors:
-            return False
-        
+        print("Colors: ", colors)
         return colors
-    else:
-        print(msg.data)
-        pattern = re.compile(r'\b(yellow|green|red|blue)\b')
-        matches = re.findall(pattern, msg.data)
-        
-        for match in matches:
-            color = match.split()[0]
-            colors.append(color)
-
-        if not colors:
-            return False
     
-    return colors
+def main():
 
-rospy.init_node("voice_recognition_node")
-colors_list = recognize_voice()
-print(colors_list)
+    vd = voice_detector()
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+
+if __name__ == "__main__":
+    main()
+
